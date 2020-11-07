5f64155bcfe366735466fb51dcfbce8f5b8105f4
Lira <l.lirajw@hotmail.com>
2020-11-07 07:16:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repository, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {    
+      setRepository(...repository, response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post('repositories', {
+        title: `Desafio React ${Date.now()}`,
+        url: "https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs", 
+        techs: ["Node.js", "REACT"]
+      });
+
+      console.log(response);
+
+      setRepository(repository => [...repository, response.data]);
+
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+
+      await api.delete(`repositories/${id}`);
+
+      const repoDelete = [...repository];
+      const index = repoDelete.findIndex(repo => repo.id == id);
+      repoDelete.splice(index, 1);
+
+      setRepository(repoDelete);
+      
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          
+            repository.map((repo) => ( 
+              
+              <li key={repo.id}>
+                      {repo.title}
+                      <button onClick={() => handleRemoveRepository(repo.id)}>
+                        Remover
+                      </button>
+            </li>))            
+        }
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
